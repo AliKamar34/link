c85f32f13621a299743f93461e2e82cd244e4b92
AliKamar34 <aliekamar123@gmail.com>
2025-11-24 13:41:27 -0800

    create custom app bar

---
+++ lib/features/companies/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:link_task/core/theme/app_color.dart';
+import 'package:link_task/core/theme/app_text_styles.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key, required this.icon, this.onTap});
+
+  final String icon;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 84,
+      decoration: BoxDecoration(
+        color: AppColor.appBarColor,
+        borderRadius: BorderRadiusDirectional.only(
+          bottomEnd: .circular(16),
+          bottomStart: .circular(16),
+        ),
+      ),
+      child: Padding(
+        padding: .symmetric(horizontal: 16),
+        child: Row(
+          mainAxisAlignment: .spaceBetween,
+          children: [
+            Text(
+              'الشركات والأفراد',
+              style: AppTextStyle.styleRegular16.copyWith(
+                color: AppColor.textColor,
+              ),
+            ),
+            InkWell(onTap: onTap, child: SvgPicture.asset(icon, width: 30)),
+          ],
+        ),
+      ),
+    );
+  }
+}
