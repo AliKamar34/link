2a0f288f7440ea7c52cc932f703808f7ee0d9cd8
AliKamar34 <aliekamar123@gmail.com>
2025-11-23 22:09:00 -0800

    create app test styles and app colors

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const appBarColor = Color(0xffEFEFEF);
+  static const textFeildColor = Color(0xffFBFBFB);
+  static const darkGreyColor = Color(0xff4C4F54);
+  static const textColor = Color(0xff000000);
+  static const lightGreyColor = Color(0xffF9F9F9);
+  static const primaryColor = Color(0xff75A460);
+  static const redColor = Color(0xffEF2D28);
+  static const greyBorderColor = Color(0xffB8B8B8);
+  static const greenBorderColor = Color(0xff4E6D40);
+}
---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppTextStyle {
+  static TextStyle styleRegular16 = GoogleFonts.cairo(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleRegular12 = GoogleFonts.cairo(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleMedium12 = GoogleFonts.cairo(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleRegular8 = GoogleFonts.elMessiri(
+    fontSize: 8.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleRegular10 = GoogleFonts.elMessiri(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleMedium10 = GoogleFonts.cairo(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle styleMedium14 = GoogleFonts.elMessiri(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleMedium14Cairo = GoogleFonts.cairo(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle styleMedium16 = GoogleFonts.cairo(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleBold16 = GoogleFonts.cairo(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+  );
+}
