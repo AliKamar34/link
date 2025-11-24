90b77aab16992044d578b272bce994df03b7495d
AliKamar34 <aliekamar123@gmail.com>
2025-11-24 13:41:58 -0800

    create filtering search widget

---
+++ lib/features/companies/presentation/views/widgets/filtering_search_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:link_task/core/constants/app_assets.dart';
+import 'package:link_task/core/theme/app_color.dart';
+import 'package:link_task/features/companies/presentation/views/widgets/custom_text_field.dart';
+import 'package:link_task/features/companies/presentation/views/widgets/filtering_bottom_sheet.dart';
+
+class FilteringSearchWidget extends StatelessWidget {
+  const FilteringSearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const .symmetric(horizontal: 16),
+      child: Row(
+        spacing: 16,
+        children: [
+          Expanded(
+            child: CustomTextField(
+              hint: 'ابحث هنا عن فرد او شركة',
+              prefixIcon: SvgPicture.asset(AppAssets.searchIcon),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: .vertical(top: .circular(25.r)),
+                ),
+                builder: (_) {
+                  return FilteringBottomSheet();
+                },
+              );
+            },
+            borderRadius: .circular(28.r),
+            child: CircleAvatar(
+              radius: 28.r,
+              backgroundColor: AppColor.darkGreyColor,
+              child: SvgPicture.asset(AppAssets.filterIcon),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
