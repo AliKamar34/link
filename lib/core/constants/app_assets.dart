634a46baa25985a61b3891f1c9d6901c5a2b376e
AliKamar34 <aliekamar123@gmail.com>
2025-11-23 21:44:40 -0800

    add assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  static const String imagePath = 'assets/images';
+  static const String noResultImage = '$imagePath/no_result_image.svg';
+
+  static const String iconPath = 'assets/icons';
+  static const String filterIcon = '$iconPath/filter_icon.svg';
+  static const String grindIcon = '$iconPath/grid_icon.svg';
+  static const String heartIcon = '$iconPath/heart_icon.svg';
+  static const String mapPinIcon = '$iconPath/MapPin_icon.svg';
+  static const String menuIcon = '$iconPath/menu_icon.svg';
+  static const String redFavIcon = '$iconPath/red_fav_icon.svg';
+  static const String searchIcon = '$iconPath/search_icon.svg';
+}
