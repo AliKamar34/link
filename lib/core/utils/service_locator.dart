252d8cf24817763742e1e3213df8b51fd4ae1446
AliKamar34 <aliekamar123@gmail.com>
2025-11-24 23:16:14 -0800

    feat(core): setup dependency injection with GetIt
    - Register DioHelper
    - Register data sources, repositories, use cases

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,28 @@
+import 'package:get_it/get_it.dart';
+import 'package:link_task/core/networking/dio_helper.dart';
+import 'package:link_task/features/companies/data/datasource/remote_data_source.dart';
+import 'package:link_task/features/companies/data/repo/companies_repo_impl.dart';
+import 'package:link_task/features/companies/domain/repo/companies_repo.dart';
+import 'package:link_task/features/companies/domain/usecases/filter_companies_use_case.dart';
+import 'package:link_task/features/companies/domain/usecases/get_cities_use_case.dart';
+import 'package:link_task/features/companies/domain/usecases/get_subcategories_use_case.dart';
+
+final GetIt sl = GetIt.instance;
+
+void setupServiceLocator() {
+  // DioHelper
+  sl.registerLazySingleton(() => DioHelper());
+
+  // Data Sources
+  sl.registerLazySingleton<CompaniesRemoteDataSource>(
+    () => CompaniesRemoteDataSourceImpl(sl()),
+  );
+
+  // Repositories
+  sl.registerLazySingleton<CompaniesRepo>(() => CompaniesRepoImpl(sl()));
+
+  // Use Cases
+  sl.registerLazySingleton(() => GetCitiesUseCase(sl()));
+  sl.registerLazySingleton(() => GetSubcategoriesUseCase(sl()));
+  sl.registerLazySingleton(() => FilterCompaniesUseCase(sl()));
+}
