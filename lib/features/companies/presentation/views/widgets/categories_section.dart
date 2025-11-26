8cfe6b9ad207ce02f85fc67f6111c24622efeb3c
AliKamar34 <aliekamar123@gmail.com>
2025-11-25 22:37:54 -0800

    feat(presentation): refactor CategoriesSection and CategorySelector for improved state management and selection handling

--- lib/features/companies/presentation/views/widgets/categories_section.dart
+++ lib/features/companies/presentation/views/widgets/categories_section.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:link_task/core/theme/app_text_styles.dart';
+import 'package:link_task/features/companies/domain/entities/sub_category.dart';
 import 'package:link_task/features/companies/presentation/views/widgets/category_selector.dart';
 
 class CategoriesSection extends StatelessWidget {
-  const CategoriesSection({super.key});
-
+  const CategoriesSection({
+    super.key,
+    required this.categories,
+    required this.onSelect,
+  });
+  final List<Subcategory> categories;
+  final Function(List<int>) onSelect;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -12,7 +18,12 @@ class CategoriesSection extends StatelessWidget {
       crossAxisAlignment: .start,
       children: [
         Text('الخدمات', style: AppTextStyle.styleMedium16),
-        CategorySelector(),
+        CategorySelector(
+          categories: categories,
+          onSelect: (selectedIDS) {
+            onSelect(selectedIDS);
+          },
+        ),
       ],
     );
   }
--- lib/features/companies/presentation/views/widgets/category_selector.dart
+++ lib/features/companies/presentation/views/widgets/category_selector.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:link_task/features/companies/domain/entities/sub_category.dart';
 import 'package:link_task/features/companies/presentation/views/widgets/custom_selectable_button.dart';
 
-class CategorySelector extends StatelessWidget {
-  const CategorySelector({super.key});
+class CategorySelector extends StatefulWidget {
+  const CategorySelector({
+    super.key,
+    required this.categories,
+    required this.onSelect,
+  });
+  final List<Subcategory> categories;
+  final Function(List<int>) onSelect;
 
   @override
+  State<CategorySelector> createState() => _CategorySelectorState();
+}
+
+class _CategorySelectorState extends State<CategorySelector> {
+  List<int> selectedIds = [];
+  @override
   Widget build(BuildContext context) {
-    final List<String> categories = [
-      'إستشارات هندسية',
-      'لاند سكيب',
-      'تصاميم واجهات خارجية',
-      'تصاميم داخلية',
-      'مساحة',
-      'إشراف',
-    ];
     return Wrap(
       spacing: 12,
       runSpacing: 12,
-      children: List.generate(categories.length, (index) {
+      children: List.generate(widget.categories.length, (index) {
         return CustomSelectableButton(
-          text: categories[index],
-          isSelected: false,
+          onTap: () {
+            setState(() {
+              int id = widget.categories[index].id;
+
+              if (selectedIds.contains(id)) {
+                selectedIds.remove(id);
+              } else {
+                selectedIds.add(id);
+              }
+            });
+            widget.onSelect(selectedIds);
+          },
+          text: widget.categories[index].name,
+          isSelected: selectedIds.contains(widget.categories[index].id),
         );
       }),
     );
